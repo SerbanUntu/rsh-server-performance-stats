4a302e524857bb9684016d492917b5fbb72def02
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-03-23 12:39:45 +0100

    refactor: clean up used cpu code, add shebang

--- server-stats.sh
+++ server-stats.sh
@@ -1,3 +1,5 @@
-USED_CPU=$(echo "100 - $(top -n 1 | head -n 3 | tail -n 1 | cut -d ',' -f 4 | tr -d " id" | cut -c 17-20)" | bc)
+#!/bin/bash
+FREE_CPU=$(top -n 1 | head -n 3 | tail -n 1 | cut -d ',' -f 4 | tr -d " id" | cut -c 17-20)
+USED_CPU=$(echo "100 - ${FREE_CPU}" | bc)
 echo "Total CPU Usage: ${USED_CPU}%"
 
