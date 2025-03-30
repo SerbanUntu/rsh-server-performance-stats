9389b5689f6c9eb7d4d474916bdcfa534ad68d54
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-03-30 12:21:36 +0200

    feat: show top 5 user agents in nginx log
    - Also add the shebang
    - Also simplify the code by reusing the sorting function

--- nginx-log-analyser/analyse-log.sh
+++ nginx-log-analyser/analyse-log.sh
@@ -1,16 +1,27 @@
+#!/bin/bash
 FORMAT_STR='{ print $2 " - " $1 " requests"}'
 
-TOP_ADDRESSES=$(awk '{ print $1 }' ./logfile.txt | sort | uniq -c | sort -nr | head -n5)
-FORMATTED_ADDRESSES=$(awk "${FORMAT_STR}" <<< "${TOP_ADDRESSES}")
+get_top_five () {
+  TOP_FIVE=$(sort <<< "$1" | uniq -c | sort -nr | head -n5)
+}
+
+ADDRESSES=$(awk '{ print $1 }' ./logfile.txt)
+get_top_five "${ADDRESSES}"
+FORMATTED_ADDRESSES=$(awk "${FORMAT_STR}" <<< "${TOP_FIVE}")
 printf "Top 5 IP addresses with the most requests:\n%s\n\n" "${FORMATTED_ADDRESSES}"
 
-REQ_LINES=$(cut -d'"' -f 2 ./logfile.txt)
-TOP_PATHS=$(awk '{ print $2 }' <<< "${REQ_LINES}" | sort | uniq -c | sort -nr | head -n5)
-FORMATTED_PATHS=$(awk "${FORMAT_STR}" <<< "${TOP_PATHS}")
+PATHS=$(cut -d'"' -f 2 ./logfile.txt | awk '{ print $2 }')
+get_top_five "${PATHS}"
+FORMATTED_PATHS=$(awk "${FORMAT_STR}" <<< "${TOP_FIVE}")
 printf "Top 5 most requested paths:\n%s\n\n" "${FORMATTED_PATHS}"
 
 STATUS_CODES=$(cut -d'"' -f 3 ./logfile.txt | awk '{ print $1 }')
-TOP_STATUSES=$(sort <<< "${STATUS_CODES}" | uniq -c | sort -nr | head -n5)
-FORMATTED_STATUSES=$(awk "${FORMAT_STR}" <<< "${TOP_STATUSES}")
-printf "Top 5 response status codes:\n%s\n" "${FORMATTED_STATUSES}"
+get_top_five "${STATUS_CODES}"
+FORMATTED_STATUSES=$(awk "${FORMAT_STR}" <<< "${TOP_FIVE}")
+printf "Top 5 response status codes:\n%s\n\n" "${FORMATTED_STATUSES}"
+
+USER_AGENTS=$(cut -d'"' -f 6 ./logfile.txt)
+get_top_five "${USER_AGENTS}"
+FORMATTED_AGENTS=$(awk '{ for (i=2; i<=NF; i++) printf $i " "; print "- " $1 " requests"}' <<< "${TOP_FIVE}")
+printf "Top 5 user agents:\n%s\n" "${FORMATTED_AGENTS}"
 
