20e53d6170451367598a7fd298e05825c37b025f
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-03-30 11:46:13 +0200

    feat: show top 5 paths in nginx log
    Also replace "uniq -c" with "sort | uniq -c" to take into account
    repetitions that are not adjacent.

--- nginx-log-analyser/analyse-log.sh
+++ nginx-log-analyser/analyse-log.sh
@@ -1,3 +1,9 @@
-TOP_ADDRESSES=$(awk '{ print $1 }' ./logfile.txt | uniq -c | sort -nr | head -n5)
+TOP_ADDRESSES=$(awk '{ print $1 }' ./logfile.txt | sort | uniq -c | sort -nr | head -n5)
 FORMATTED_ADDRESSES=$(awk '{ print $2 " - " $1 " requests"}' <<< "${TOP_ADDRESSES}")
-printf "Top 5 IP addresses with the most requests:\n%s\n" "${FORMATTED_ADDRESSES}"
+printf "Top 5 IP addresses with the most requests:\n%s\n\n" "${FORMATTED_ADDRESSES}"
+
+REQ_LINES=$(cut -d'"' -f 2 ./logfile.txt)
+TOP_PATHS=$(awk '{ print $2 }' <<< "${REQ_LINES}" | sort | uniq -c | sort -nr | head -n5)
+FORMATTED_PATHS=$(awk '{ print $2 " - " $1 " requests"}' <<< "${TOP_PATHS}")
+printf "Top 5 most requested paths:\n%s\n" "${FORMATTED_PATHS}"
+
